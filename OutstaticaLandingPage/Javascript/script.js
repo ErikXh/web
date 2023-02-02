017b94f9d2e315aab7163dd1eea1cf5b006a5a01
ErikXh <erikxheleshi@gmail.com>
2023-02-02 19:38:37 +0100

    Carousel update

--- OutstaticaLandingPage/Javascript/script.js
+++ OutstaticaLandingPage/Javascript/script.js
@@ -7,8 +7,43 @@ menuBtn.addEventListener('click', ()=>{
     navMenu.classList.toggle("showMenu");
 })
 
-// carousel
+// Test carousel
 
-// We need images
+const carouselImg = document.getElementById('carousel');
+const arrowLeft = document.getElementsByClassName("move-left--js");
+const arrowRight = document.getElementsByClassName("move-right--js");
 
-const images = []
+arrowLeft.addEventListener('click', ()=>{
+    changeBackgroundImage(-1);
+})
+
+arrowRight.addEventListener('click', ()=>{
+    changeBackgroundImage(+1)
+})
+
+const images = [
+'C:\git\web\OutstaticaLandingPage\img\resized_team_image_test.jpg',
+'C:\git\web\OutstaticaLandingPage\img\resized_team_image_test.jpg',
+'C:\git\web\OutstaticaLandingPage\img\resized_team_image_test.jpg'
+]
+
+
+function changeBackgroundImage(num){
+
+if(num < 0){
+carouselImg.style.backgroundImage = images[2];
+}
+
+if(num == 0){
+carouselImg.style.backgroundImage = images[0];
+}
+
+if(num == 1){
+carouselImg.style.backgroundImage = images[1];
+}
+
+if(num == 2){
+carouselImg.style.backgroundImage = images[2];
+}
+     
+}
