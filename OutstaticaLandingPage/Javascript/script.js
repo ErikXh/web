e1cc662221c15176d3fd402d9e09709257be0af4
ErikXh <erikxheleshi@gmail.com>
2023-02-06 22:32:42 +0100

    newsletter section added, carousel additional test

--- OutstaticaLandingPage/Javascript/script.js
+++ OutstaticaLandingPage/Javascript/script.js
@@ -13,46 +13,19 @@ const carouselImg = document.getElementById('carousel');
 const arrowLeft = document.getElementsByClassName("move-left--js");
 const arrowRight = document.getElementsByClassName("move-right--js");
 
-//  arrowLeft.addEventListener('click', ()=>{
-//      changeBackgroundImage(-1);
-//  })
-
-// arrowRight.addEventListener('click', ()=>{
-//     changeBackgroundImage(+1)
-// })
-
 const images = [
 'https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 'https://images.pexels.com/photos/3183186/pexels-photo-3183186.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 'https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
 ]
 
-//console.log(images[1])
+carouselImg.style.background = 'https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
 
-arrowLeft.addEventListener('click', ()=>{
-    carouselImg.style.backgroundImage = "https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
-})
+// console.log(images[0]); the links are displayed in the console
 
+// this function changes the image based on on the index of the array "images" above
 
-
-
-
-// function changeBackgroundImage(num){
-
-// if(num < 0){
-// carouselImg.style.backgroundImage = images[2];
-// }
-
-// if(num == 0){
-// carouselImg.style.backgroundImage = images[0];
-// }
-
-// if(num == 1){
-// carouselImg.style.backgroundImage = images[1];
-// }
-
-// if(num == 2){
-// carouselImg.style.backgroundImage = images[2];
-// }
-     
-// }
+function changeImage(num){
+   
+return null;
+}
