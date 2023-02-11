a5904005a71efdfc86d89a322130d326f66090e8
ErikXh <erikxheleshi@gmail.com>
2023-02-11 13:25:05 +0100

    image slider fixed

--- OutstaticaLandingPage/Javascript/script.js
+++ OutstaticaLandingPage/Javascript/script.js
@@ -18,45 +18,37 @@ const images = [
 'https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
 ]
 
-//carouselImg.style.background="url(link)";
-
-// console.log(images[0]); the links are displayed in the console
-
 // this function changes the image based on on the index of the array "images" above
 
- function changeImage(num){
+let currentIndex = 2;
 
- if(num == 0){
-     carouselImg.style.background="url('" + images[0] + "')";
- }
+function right(){
+    if(currentIndex == 2){
+        currentIndex = 0;
+    }
+    else{
+        currentIndex++;
+    }
+    carouselImg.style.backgroundImage="url('" + images[currentIndex] + "')";
+}
 
- if(num == 1){
-     carouselImg.style.background="url('" + images[1] + "')";
- }
-
- if(num == 2){
-     carouselImg.style.background="url('" + images[2] + "')";
- }
-
- if(num > images.length){
-     carouselImg.style.background="url('" + images[0] + "')";
- }
-
- if(num < 0){
-     carouselImg.style.background="url('" + images[2] + "')";
- }
-
- }
-
-// left/ right arrow events
+function left(){
+    if(currentIndex == 0){
+        currentIndex = 2;
+    }
+    else{
+        currentIndex--;
+    }
+    carouselImg.style.backgroundImage="url('" + images[currentIndex] + "')";
+}
 
 const arrowLeft = document.querySelector(".move-left--js");
 const arrowRight = document.querySelector(".move-right--js");
 
 arrowRight.addEventListener('click', ()=>{
-    changeImage(-1)
+    right();
 })
 
 arrowLeft.addEventListener('click', ()=>{
-    changeImage(+1)
+    left();
 })
