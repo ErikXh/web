9e8b0c1a28b7a5867a3bd656141aea9f726cc86c
ErikXh <erikxheleshi@gmail.com>
2023-02-11 13:29:34 +0100

    image slider fix

--- OutstaticaLandingPage/Javascript/script.js
+++ OutstaticaLandingPage/Javascript/script.js
@@ -23,23 +23,23 @@ const images = [
 let currentIndex = 2;
 
 function right(){
-    if(currentIndex == 2){
-        currentIndex = 0;
-    }
-    else{
-        currentIndex++;
-    }
-    carouselImg.style.backgroundImage="url('" + images[currentIndex] + "')";
+if(currentIndex == 2){
+currentIndex = 0;
+}
+else{
+currentIndex++;
+}
+carouselImg.style.backgroundImage="url('" + images[currentIndex] + "')";
 }
 
 function left(){
-    if(currentIndex == 0){
-        currentIndex = 2;
-    }
-    else{
-        currentIndex--;
-    }
-    carouselImg.style.backgroundImage="url('" + images[currentIndex] + "')";
+if(currentIndex == 0){
+currentIndex = 2;
+}
+else{
+currentIndex--;
+}
+carouselImg.style.backgroundImage="url('" + images[currentIndex] + "')";
 }
 
 const arrowLeft = document.querySelector(".move-left--js");
